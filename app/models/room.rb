e7cbe0256ac99f395b390ca6e6f3f51e1e2f0a09
ddd444g <daiki.nakamura.424@icloud.com>
2022-12-12 01:30:43 +0900

    画像完了、アイコン完了、予約モデル未完、検索機能未完、バリデーション未完、デザイン未完

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,4 +1,5 @@
 class Room < ApplicationRecord
   belongs_to :user, optional: true
   validates :name, presence: true
+  mount_uploader :img, ImgUploader
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,4 @@
 class User < ApplicationRecord
   has_many :rooms
+  mount_uploader :icon, ImgUploader
 end
